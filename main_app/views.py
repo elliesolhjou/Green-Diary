48ae207e71d6e9790d1c002321ffda5d93c674c8
Tim Huitt <timhuitt@gmail.com>
2024-01-14 00:19:39 -0700

    update trips styling

--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 from django.conf import settings 
 
 def home(request):
-    json_file_path = os.path.join(settings.BASE_DIR, 'main_app/static/data/vehicles.json')
+    vehicles_path = os.path.join(settings.BASE_DIR, 'main_app/static/data/vehicles.json')
+    trips_path = os.path.join(settings.BASE_DIR, 'main_app/static/data/trips.json')
 
-    with open(json_file_path, 'r') as json_data:
+    with open(trips_path, 'r') as json_data:
+        trip_data = json.load(json_data)
+
+    with open(vehicles_path, 'r') as json_data:
         vehicle_data = json.load(json_data)
 
-    return render(request, 'home.html', {'vehicles': vehicle_data})
+    return render(request, 'home.html', {'vehicles': vehicle_data, 'trips': trip_data})

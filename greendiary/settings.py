016d0c0dba219dc69376683879b0739d4176c480
Tim Huitt <timhuitt@gmail.com>
2024-01-17 11:10:08 -0700

    add heroku settings

--- greendiary/settings.py
+++ greendiary/settings.py
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())

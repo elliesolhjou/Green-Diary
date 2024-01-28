7233172d18fb8ccf07be5535812185f19c134072
Ellie Solhjou <elliesolhjou@gmail.com>
2024-01-28 17:42:15 -0600

    Initiated utility functions

---
+++ greendiary/utility.py
@@ -0,0 +1,10 @@
+# utility functions here
+from django.conf import settings
+from django.shortcuts import redirect
+from urllib.parse import urlencode
+import requests
+import json
+import datetime
+from humanfriendly import format_timespan
+from django.http import JsonResponse
+

4e80fc6dff0c12980a91d0582c26b26971bdc337
Ellie Solhjou <elliesolhjou@gmail.com>
2024-01-29 12:15:22 -0600

    updated user view

--- users/views.py
+++ users/views.py
@@ -19,10 +19,33 @@ message= "There was an error, please try again"
 # It is responsible for taking an HTTP request and dispatching it to the appropriate handler 
 # method within the class based on the HTTP method (e.g., GET, POST, PUT, DELETE). 
 
-class AccountView(TemplateView):
-    template_name = "users/account.html"
+# to view profile
+class profileView(TemplateView):
+    template_name = "users/profile.html"
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         # Call the superclass method
         # dispatch is CRUD HANDLER -> The dispatch method examines the HTTP method of the request (in CBV)
+        #  in CBV, if you are overwriting methods, should do super().overwriteMethod()
         return super().dispatch(*args, **kwargs)
+    
+
+# to update profile
+class UpdateUser(UpdateView):
+# def  UpdateUser(request):
+    model = UserProfile
+    fields = '__all__'
+    # if request.method =='POST':
+            
+    #     form = UserProfileForm(request.POST)
+    #     if form.is_valid():
+    #         user_profile = form.save()
+    #         user_profile_has_profile = True
+    #         result='success' 
+    #         message='Your profile has been updated successfully!'
+    #     else:
+    #         message= " Invalid. Please try again!"
+    #         print("user profile update unsuccessful", form.errors)
+                
+
+
